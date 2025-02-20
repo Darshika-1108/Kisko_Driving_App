f29f21862b78a4e1750fb91b6c974195fdc69fbf
Darshika-1108 <pateldarshikaj@gmail.com>
2025-02-19 22:04:54 -0500

    Updated

--- controllers/updateG.js
+++ controllers/updateG.js
@@ -1,3 +1,4 @@
+const Appointment = require('../models/Appointment');
 const DriveTest = require('../models/DriveTest');
 
 // Update Car Information
@@ -19,11 +20,19 @@ module.exports = async (req, res) => {
             { new: true }
         );
 
-        if (!user) {
-            return res.render('g', { user: null, error: 'No User Found' });
+        // Fetch the user's appointment details
+        const appointment = await Appointment.findById(user.appointment);
+
+         // Convert date to Date object if it's not already
+         if (appointment && typeof appointment.date === 'string') {
+            appointment.date = new Date(appointment.date);
         }
 
-        res.render('g', { user, error: null });
+        if (!user) {
+            return res.render('g', { user: null, appointment : null, error: 'No User Found' });
+        }
+
+        res.render('g', { user, appointment, error: null });
     } catch (error) {
         console.log(error);
         res.render('g', { error: 'Error updating car information', user: null });
